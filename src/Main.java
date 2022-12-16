f3504a798ea4829e63fbda45664ed47ebb57c964
TamaraZolotovskaya <tamarafx25@gmail.com>
2022-12-16 13:56:39 +0700

    Исключения. Задача проверки логина и пароля

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+
     public static void main(String[] args) {
+
+        LoginAndPass first = new LoginAndPass("tamara2_", "hahaha", "hahaрha");
+
+        try {
+            LoginAndPass.IsLogPassTrue(first.getLogin(), first.getPassword(), first.getConfirmPassword());
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
+
+
 
     }
 }
